4e9feaee4384ab8626a7169f8bfa8cf6a1691fec
Caitlin Ashtari and Marty Kovach <caitlin@>
2016-11-14 16:52:10 -0800

    End of class commit. stupid sassafras

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -6,6 +6,17 @@ class String
     leet_array.each() do |letter|
       if letter.eql?("e")
         speak_array.push("3")
+      elsif letter.eql?("o")
+        speak_array.push("0")
+      elsif letter.eql?("I")
+        speak_array.push("1")
+
+      # elsif letter.eql?(leet_array.at(0))
+      #   speak_array.push("s")
+
+    elsif letter.eql?("s") && leet_array.index > 0
+        speak_array.push("z")
+
       else
         speak_array.push(letter)
       end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -8,5 +8,17 @@ describe('String#leetspeak') do
   it('replaces every "e" in a string with a "3"') do
     expect("the".leetspeak).to(eq('th3'))
   end
+  it("replaces every 'o' in a string with a '0'") do
+    expect("boot".leetspeak).to(eq("b00t"))
+  end
+  it("replaces every 'I' in a string with a '1'") do
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+  end
+  it("replaces every 's' in a string with a 'z'") do
+    expect("roses".leetspeak).to(eq("r0z3z"))
+  end
+  it("does NOT replace the first letter when it is an s") do
+    expect("sassafrass".leetspeak).to(eq("sazzafrazz"))
+  end
 
 end
